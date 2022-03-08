2d0ff721e892c7427eea39191a15047408c962ab
Lukas <boehm-lukas@outlook.de>
2022-03-08 23:52:31 +0000

    :wrench:

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source ./server/venv/bin/activate
-pip install -r requirements
+pip install -r ./server/requirements.txt
 export FLASK_DEBUG=1
 export API_KEY=123qwe
 export FLASK_APP=./server/server.py
