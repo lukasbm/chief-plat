92c92eb6b1d941bfdd34bdfb2c3f302e04b619c3
Lukas <boehm-lukas@outlook.de>
2022-03-09 20:07:34 +0000

    :art: minior ui fixes

--- web/src/components/Header.vue
+++ web/src/components/Header.vue
@@ -1,15 +1,15 @@
 <script setup>
-import AuthVue from './Auth.vue';
-import DarkModeVue from './DarkMode.vue';
+import AuthVue from "./Auth.vue";
+import DarkModeVue from "./DarkMode.vue";
 </script>
 
 <template>
   <nav class="bg-gray-150 shadow-md border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-800 mb-4">
     <div class="container flex flex-wrap justify-between items-center mx-auto">
-      <a href="#" class="flex items-center">
+      <div class="flex items-center">
         <img src="/favicon.ico" class="mr-3 h-6 sm:h-10" alt="Flowbite Logo" />
         <h1 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Chief-Plat</h1>
-      </a>
+      </div>
       <div class="flex md:order-2">
         <AuthVue></AuthVue>
         <DarkModeVue></DarkModeVue>
--- web/src/components/Project.vue
+++ web/src/components/Project.vue
@@ -5,12 +5,10 @@ defineProps({
 </script>
 
 <template>
-  <div class="p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-    <a href="#">
-      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-        {{ project.name }}
-      </h5>
-    </a>
+  <div class="p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 space-y-2">
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+      {{ project.name }}
+    </h5>
 
     <button
       type="button"
