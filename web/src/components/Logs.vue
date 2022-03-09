eebdf97579862a0e7bae4f104943a82e20da6efb
Lukas <boehm-lukas@outlook.de>
2022-03-09 02:52:48 +0000

    :sparkles: start page

--- web/src/components/Logs.vue
+++ web/src/components/Logs.vue
@@ -1,10 +1,26 @@
 <script setup>
-defineProps({
-  logs: {
-    type: Array,
-    required: true,
+const logs = [
+  {
+    container: "redis",
+    text: `
+1:C 05 Mar 2022 17:58:55.865 # oO0OoO0OoO0Oo Redis is starting oO0OoO0OoO0Oo
+1:C 05 Mar 2022 17:58:55.865 # Redis version=6.2.6, bits=64, commit=00000000, modified=0, pid=1, just started
+1:C 05 Mar 2022 17:58:55.865 # Warning: no config file specified, using the default config. In order to specify a config file use redis-server /path/to/redis.conf
+1:M 05 Mar 2022 17:58:55.865 * monotonic clock: POSIX clock_gettime
+1:M 05 Mar 2022 17:58:55.866 * Running mode=standalone, port=6379.
+1:M 05 Mar 2022 17:58:55.866 # Server initialized
+1:M 05 Mar 2022 17:58:55.866 # WARNING overcommit_memory is set to 0! Background save may fail under low memory condition. To fix this issue add 'vm.overcommit_memory = 1' to /etc/sysctl.conf and then reboot or run the command 'sysctl vm.overcommit_memory=1' for this to take effect.
+1:M 05 Mar 2022 17:58:55.866 * Ready to accept connections
+`,
   },
-});
+  {
+    container: "nginx",
+    text: `
+hallo
+lol
+    `,
+  },
+];
 </script>
 
 <template>
