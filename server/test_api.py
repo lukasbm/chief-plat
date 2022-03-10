e203b69008838df4c0a1e75d0ff9e12f47111977
Lukas <boehm-lukas@outlook.de>
2022-03-10 04:30:46 +0000

    :bug: fix tests

--- server/test_api.py
+++ server/test_api.py
@@ -1,11 +1,31 @@
 import pytest
 from api import app
+from werkzeug.wrappers import Response
 
 @pytest.fixture()
-def client(app):
+def client():
     return app.test_client()
 
 
 @pytest.fixture()
-def runner(app):
+def runner():
     return app.test_cli_runner()
+
+def test_unauthorized(client):
+    resp: Response = client.get("/projects")
+    assert resp.status_code == 401
+    assert "Unauthorized" in resp.get_data(as_text=True)
+
+# TODO auth
+
+def test_projects(client):
+    resp: Response = client.get("/projects")
+
+def test_project_start(client):
+    resp = client.get("/project/test-app/start")
+
+def test_project_stop(client):
+    resp = client.get("/project/test-app/stop")
+
+def test_project_logs(client):
+    resp = client.get("/project/test-app/logs")
--- server/test_project.py
+++ server/test_project.py
@@ -1,16 +1,19 @@
-from project import Project
+from project import Project, all_projects, find_project
 
-class ProjectTest:
-    def test_get_logs():
-        pass
 
-    def test_get_status():
-        pass
+def test_get_logs():
+    pass
 
-    def test_all_projects():
-        pass
 
-    def test_find_project():
-        p = Project.find_project("test-app")
-        assert p.name == "test-app"
-        assert p.containers == ["test-app-redis"]
+def test_get_status():
+    pass
+
+
+def test_all_projects():
+    pass
+
+
+def test_find_project():
+    p = find_project("test-app")
+    assert p.name == "test-app"
+    assert p.containers == ["test-app-redis"]
